0f4de16addc66a8dab6cc4708a26b78fb55217e7
marklo0508 <95956782+marklo0508@users.noreply.github.com>
2025-05-07 11:22:29 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,15 @@
+{
+    "name": "Air Sensor out",
+    "description": "",
+    "dependencies": {
+        "core": "*",
+        "radio": "*",
+        "microphone": "*"
+    },
+    "files": [
+        "main.blocks",
+        "main.ts",
+        "README.md"
+    ],
+    "preferredEditor": "tsprj"
+}
